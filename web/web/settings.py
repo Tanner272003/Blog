2067acf5ff501a2b4099826e282f704a92b32235
enockglidden <enockglidden@hotmail.com>
2016-02-18 18:11:51 -0500

    trying to fix error

--- web/web/settings.py
+++ web/web/settings.py
@@ -83,10 +83,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 # Application definition
 
 INSTALLED_APPS = (
-    'mysite',
+    # 'mysite',
     'django.contrib.admin',
     'django.contrib.auth',
-    # 'django.contrib.contenttypes',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
