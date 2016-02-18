2bc6fe684e5886c145f1399a06908f5f2aa485bb
enockglidden <enockglidden@hotmail.com>
2016-02-18 16:44:01 -0500

    changing media url

--- web/web/settings.py
+++ web/web/settings.py
@@ -150,7 +150,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/media/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
