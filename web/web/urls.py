8cb3542a49cd4f92baa41c2ae2d652ebb4500884
enockglidden <enockglidden@hotmail.com>
2016-02-29 16:03:52 -0500

    fixing about url

--- web/web/urls.py
+++ web/web/urls.py
@@ -36,7 +36,7 @@ from zinnia.sitemaps import AuthorSitemap
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/blog/', permanent=True)),
+    url(r'^$', RedirectView.as_view(url='/about/', permanent=True)),
     url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
 	url(r'^blog/', include('zinnia.urls.entries')),
     url(r'^comments/', include('django_comments.urls')),
